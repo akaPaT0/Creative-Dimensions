d78764bd9eac712c1ae21192ab0c740f79cb1307
akaPaT0 <justmyemaile@gmail.com>
2026-01-25 23:03:15 +0200

    Edit product: KECU001

--- app/data/products.ts
+++ app/data/products.ts
@@ -29,7 +29,7 @@ export const products: Product[] = [
       "/products/Cute_Crab_1.webp",
       "/products/Cute_Crab_2.webp"
     ],
-    "isNew": true,
+    "isNew": false,
     "featured": true
   },
   {
